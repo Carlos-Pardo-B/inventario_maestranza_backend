bdc9c1bf2d16d4f893884f668eb67e91e1259582
PAAWSA02LC1015 <PAAWSA02LC1015@PAAWSA02LC1015.duoc.acad>
2025-06-17 19:47:41 -0400

    Union de apps y constante de modelo de usuario

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Componente(models.Model):
+    nombre = models.CharField(max_length = 50)
+    descripcion = models.TextField()
+    numero_serie = models.IntegerField(default=0)
+    ubicacion = models.TextField()
+    stock = models.IntegerField(default=0)
